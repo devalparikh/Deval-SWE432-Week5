8d771e92bf0e805fcca28b4d00d3ff0a7a271c97
Deval Parikh <devalpp@gmail.com>
2020-10-13 17:13:34 -0400

    fixed table

--- src/main/java/servlet/PersistenceFile.java
+++ src/main/java/servlet/PersistenceFile.java
@@ -85,7 +85,7 @@ public class PersistenceFile extends HttpServlet{
      if (error.length() == 0){
        PrintWriter entriesPrintWriter =
           new PrintWriter(new FileWriter(RESOURCE_FILE, true), true);
-       entriesPrintWriter.println(name+VALUE_SEPARATOR+VALUE_SEPARATOR+hobby+age);
+       entriesPrintWriter.println(name+VALUE_SEPARATOR+hobby+VALUE_SEPARATOR+age);
        entriesPrintWriter.close();
 
        printHead(out);
